57a6f43ac22f138e602df1986261a240a478210a
keresmi <>
2017-10-17 18:07:14 +0200

    test commit

---
+++ app/src/main/java/com/keresmi/forgetmenot/App.java
@@ -0,0 +1,18 @@
+package com.keresmi.forgetmenot;
+
+import android.app.Application;
+
+
+/**
+ * Created by keresmi.
+ * https://github.com/keresmi
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        //init sth here
+    }
+}
