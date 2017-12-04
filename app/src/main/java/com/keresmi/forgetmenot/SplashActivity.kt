43ac205507320249cb8259615d79661a4b936668
Mos <bartoszmos@gmail.com>
2017-12-04 22:09:58 +0100

    splash screen implemented

--- app/src/main/java/com/keresmi/forgetmenot/SplashActivity.kt
+++ app/src/main/java/com/keresmi/forgetmenot/SplashActivity.kt
@@ -1,23 +1,15 @@
 package com.keresmi.forgetmenot
 
-import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.os.Handler
-import android.content.Intent
+import android.os.Bundle
+import android.content.Intent;
 
 class SplashActivity : AppCompatActivity() {
 
-    var SPLASH_TIME_OUT = 2000
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        Handler().postDelayed( object:Runnable{
-            public override fun run() {
-                val home = Intent(this@SplashActivity,MainActivity::class.java)
-                startActivity(home)
-                finish()
-            }
-        }, SPLASH_TIME_OUT.toLong() )
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
     }
 }
